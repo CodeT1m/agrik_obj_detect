c3dae8635a465b8d87536433cdd26fb0a2c04256
CodeT1m <timmygenius17@gmail.com>
2020-06-24 01:01:09 +0800

    modify imgs

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -32,75 +32,75 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'as':
+    if row_label == 'abr':
         return 1
-    elif row_label == 'abr':
+    elif row_label == 'ah':
         return 2
     elif row_label == 'ar':
         return 3
-    elif row_label == 'ah':
+    elif row_label == 'as':
         return 4
     elif row_label == 'ch':
         return 5
     elif row_label == 'cpm':
         return 6
-    elif row_label == 'mLs':
-        return 7
-    elif row_label == 'mcr':
-        return 8
-    elif row_label == 'mh':
-        return 9
-    elif row_label == 'mLb':
-        return 10
-    elif row_label == 'g_br':
-        return 11
-    elif row_label == 'g_ebm':
-        return 12
-    elif row_label == 'grape_h':
-        return 13
     elif row_label == 'g_Lb':
+        return 7
+    elif row_label == 'g_br':
+        return 8
+    elif row_label == 'g_ebm':
+        return 9
+    elif row_label == 'grape_h':
+        return 10
+    elif row_label == 'mLb':
+        return 11
+    elif row_label == 'mLs':
+        return 12
+    elif row_label == 'mcr':
+        return 13
+    elif row_label == 'mh':
         return 14
     elif row_label == 'pbs':
         return 15 
     elif row_label == 'peach_h':
         return 16 
-    elif row_label == 'pp_bs':
-        return 17 
-    elif row_label == 'pp_h':
-        return 18 
-    elif row_label == 'po_eb':
-        return 19 
-    elif row_label == 'potato_h':
-        return 20 
     elif row_label == 'po_Lb':
+        return 17 
+    elif row_label == 'po_eb':
+        return 18 
+    elif row_label == 'potato_h':
+        return 19 
+    elif row_label == 'pp_bs':
+        return 20 
+    elif row_label == 'pp_h':
         return 21 
     elif row_label == 'rasp_h':
         return 22 
-    elif row_label == 'squ_pm':
+    elif row_label == 'sberry_Ls':
         return 23 
     elif row_label == 'sberry_h':
         return 24 
-    elif row_label == 'sberry_Ls':
+    elif row_label == 'squ_pm':
         return 25 
-    elif row_label == 'tom_bs':
-        return 26 
-    elif row_label == 'tom_eb':
-        return 27 
-    elif row_label == 'tom_h':
-        return 28 
-    elif row_label == 'tom_Lb':
-        return 29 
-    elif row_label == 'tom_Lm':
-        return 30 
     elif row_label == 't_sLs':
-        return 31 
+        return 26 
     elif row_label == 't_smt':
-        return 32 
-    elif row_label == 't_ts':
-        return 33 
+        return 27 
     elif row_label == 't_tmv':
-        return 34
+        return 28 
+    elif row_label == 't_ts':
+        return 29 
     elif row_label == 't_ty':
+        return 30 
+    elif row_label == 'tom_Lb':
+        return 31 
+    elif row_label == 'tom_Lm':
+        return 32 
+    elif row_label == 'tom_bs':
+        return 33 
+    elif row_label == 'tom_eb':
+        return 34
+    elif row_label == 'tom_h':
         return 35
     
             
